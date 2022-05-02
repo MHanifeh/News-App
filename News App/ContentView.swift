5034032caa5dfd9e632154b2ab2d8a322431b876
Mahdi.Hanifeh <hanifehmahdi3@gmail.com>
2022-05-02 23:47:08 +0430

    Initial Commit

---
+++ News App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News App
+//
+//  Created by MahdiHanifeh on 2/12/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News App/News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_AppApp.swift
+//  News App
+//
+//  Created by MahdiHanifeh on 2/12/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
